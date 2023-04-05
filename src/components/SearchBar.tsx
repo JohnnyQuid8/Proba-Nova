14e5f5b26e40a862a138c68a2fc056516109ee65
JohnnyQuid8 <johnny8bargain@gmail.com>
2023-04-06 01:14:49 +0200

    push

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,43 +2,6 @@ import { Input, Button } from "antd"
 import React from "react";
 import axios from "axios";
 
-// created
-// : 
-// "2017-11-04T18:48:46.250Z"
-// episode
-// : 
-// (51) ['https://rickandmortyapi.com/api/episode/1', 'https://rickandmortyapi.com/api/episode/2', 'https://rickandmortyapi.com/api/episode/3', 'https://rickandmortyapi.com/api/episode/4', 'https://rickandmortyapi.com/api/episode/5', 'https://rickandmortyapi.com/api/episode/6', 'https://rickandmortyapi.com/api/episode/7', 'https://rickandmortyapi.com/api/episode/8', 'https://rickandmortyapi.com/api/episode/9', 'https://rickandmortyapi.com/api/episode/10', 'https://rickandmortyapi.com/api/episode/11', 'https://rickandmortyapi.com/api/episode/12', 'https://rickandmortyapi.com/api/episode/13', 'https://rickandmortyapi.com/api/episode/14', 'https://rickandmortyapi.com/api/episode/15', 'https://rickandmortyapi.com/api/episode/16', 'https://rickandmortyapi.com/api/episode/17', 'https://rickandmortyapi.com/api/episode/18', 'https://rickandmortyapi.com/api/episode/19', 'https://rickandmortyapi.com/api/episode/20', 'https://rickandmortyapi.com/api/episode/21', 'https://rickandmortyapi.com/api/episode/22', 'https://rickandmortyapi.com/api/episode/23', 'https://rickandmortyapi.com/api/episode/24', 'https://rickandmortyapi.com/api/episode/25', 'https://rickandmortyapi.com/api/episode/26', 'https://rickandmortyapi.com/api/episode/27', 'https://rickandmortyapi.com/api/episode/28', 'https://rickandmortyapi.com/api/episode/29', 'https://rickandmortyapi.com/api/episode/30', 'https://rickandmortyapi.com/api/episode/31', 'https://rickandmortyapi.com/api/episode/32', 'https://rickandmortyapi.com/api/episode/33', 'https://rickandmortyapi.com/api/episode/34', 'https://rickandmortyapi.com/api/episode/35', 'https://rickandmortyapi.com/api/episode/36', 'https://rickandmortyapi.com/api/episode/37', 'https://rickandmortyapi.com/api/episode/38', 'https://rickandmortyapi.com/api/episode/39', 'https://rickandmortyapi.com/api/episode/40', 'https://rickandmortyapi.com/api/episode/41', 'https://rickandmortyapi.com/api/episode/42', 'https://rickandmortyapi.com/api/episode/43', 'https://rickandmortyapi.com/api/episode/44', 'https://rickandmortyapi.com/api/episode/45', 'https://rickandmortyapi.com/api/episode/46', 'https://rickandmortyapi.com/api/episode/47', 'https://rickandmortyapi.com/api/episode/48', 'https://rickandmortyapi.com/api/episode/49', 'https://rickandmortyapi.com/api/episode/50', 'https://rickandmortyapi.com/api/episode/51']
-// gender
-// : 
-// "Male"
-// id
-// : 
-// 1
-// image
-// : 
-// "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-// location
-// : 
-// {name: 'Citadel of Ricks', url: 'https://rickandmortyapi.com/api/location/3'}
-// name
-// : 
-// "Rick Sanchez"
-// origin
-// : 
-// {name: 'Earth (C-137)', url: 'https://rickandmortyapi.com/api/location/1'}
-// species
-// : 
-// "Human"
-// status
-// : 
-// "Alive"
-// type
-// : 
-// ""
-// url
-// : 
-// "https://rickandmortyapi.com/api/character/1"
-
 interface Items {
   id: number,
   name: string,
@@ -57,20 +20,10 @@ interface Items {
   makeFilterWork: string,
 }
 
-interface Filters {
-  name?: string,
-  species?: string,
-  status?: string,
-  gender?: string,
-
-}
 const SearchBar = () => {
-
   const [searchTerm, setSearchTerm] = React.useState<string>("");
   const [items, setItems] = React.useState<Items[]>([])
   const [category, setCategory] = React.useState<keyof Items>("name");
-
-
 
   React.useEffect(() => {
     async function fetchData() {
@@ -83,22 +36,21 @@ const SearchBar = () => {
       }
     }
     fetchData()
-
   }, [])
 
   const filteredItems = items.filter((item) => {
-    return item[category] === searchTerm
+    switch(category) {
+      case "name":
+      case "gender":
+      case "species":
+      case "status": {
+        return item[category].toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+      }
+      // TODO Uradi ostalo sta moze da se search
+      default: return false
+    }
   })
-
-  // const filteredItems = items.filter((item) => {
-  //     const makeFilterWork = filterState;
-  //     const filterWork = `${makeFilterWork} + `
-  //     return console.log(item.makeFilterWork)
-  // })
-  console.log(category)
-  console.log(filteredItems)
-  console.log(items)
-  
+ 
   return (
     <div>
       <Input
@@ -106,19 +58,13 @@ const SearchBar = () => {
         placeholder="Search"
         onChange={(event) => setSearchTerm(event.target.value.toLowerCase())}>
       </Input>
-      <Button onClick={() => {
-        setCategory("name")
-      }}>NAME</Button>
+      <Button onClick={() => setCategory("name")}>NAME</Button>
       <Button onClick={() => setCategory("species")}>SPECIES</Button>
       <Button onClick={() => setCategory("status")}>STATUS</Button>
       <Button onClick={() => setCategory("gender")}>GENDER</Button>
-
-
-
       {filteredItems.map((item) => {
         return <li>{item.name}</li>
       })}
-
     </div>
   )
 }
